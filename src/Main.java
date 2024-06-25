d0eff2fd284b70e1945a8ef597810fe2a5e7345e
Caleb Mwasikira <calebmwasikira@gmail.com>
2024-06-25 14:16:38 +0300

    learning java io

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,17 @@
-import java.util.Random;
-import DataStructures.Stack;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static int[] genRandomArray(int size) {
-        if(size <= 0) {
-            System.out.printf("invalid array size %s provided\n", size);
-            System.exit(1);
-        }
+    public static void main(String[] args) throws IOException {
+        String line;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter lines of text, 'stop' to quit");
 
-        int[] numbers = new int[size];
-        for(int i = 0; i < 10; i++) {
-            Random random = new Random();
-            numbers[i] = random.nextInt(100);
-        }
-        return numbers;
-    }
-
-    public static void main(String[] args) {
-        int[] numbers = genRandomArray(10);
-
-        Stack stack = new Stack(numbers.length);
-        for (int number : numbers) {
-            stack.push(number);
-        }
-        stack.items();
-
-        int num_items_to_pop = (int) (Math.random() * 10);
-        System.out.printf("popping %s items from stack\n\n", num_items_to_pop);
-        for(int i=0; i < num_items_to_pop; i++) {
-            System.out.printf("popping item %s from stack\n", stack.pop());
-            stack.items();
-            System.out.println();
-        }
+        do {
+            line = br.readLine();
+            System.out.println(line);
+        } while (!line.trim().equals("stop"));
     }
 }
 
